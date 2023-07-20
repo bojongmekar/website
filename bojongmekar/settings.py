f7ac7c2b8f78f94d5ba6ae80b368d7ca0a1cf266
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2023-07-20 12:58:02 +0700

    fix settings

--- bojongmekar/settings.py
+++ bojongmekar/settings.py
@@ -122,12 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    os.path.join(BASE_DIR, 'static'),
-]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
